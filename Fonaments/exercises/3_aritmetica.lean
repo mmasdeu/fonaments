6ce86a35ed1ce29540cbf87429897e3ad4867c39
Marc Masdeu <marc.masdeu@gmail.com>
2023-12-15 09:51:25 +0100

    Added hint for exercise

--- Fonaments/exercises/3_aritmetica.lean
+++ Fonaments/exercises/3_aritmetica.lean
@@ -5,7 +5,7 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Data.Rat.Lemmas
 import Verbose.Catalan.All
 
-open Function Finset Dvd Rat
+open Function Finset Dvd
 open scoped BigOperators
 
 
@@ -30,9 +30,13 @@ Demostració:
   exact Prime.dvd_of_dvd_pow (Nat.prime_iff.mp Nat.prime_two) h -- LEAN ja ho sap
 QED
 
+/-
+Per aquest exercici us poden ser útils els següents teoremes:
+* Nat.dvd_gcd : k ∣ m → k ∣ n → k ∣ Nat.gcd m n
+-/
 Exemple "L'arrel quadrada de 2 és irracional"
   Dades: (m n : ℕ)
-  Hipòtesis: (mn_coprimers : m.gcd n = 1)
+  Hipòtesis: (mn_coprimers : Nat.gcd m n = 1)
   Conclusió:  m^2 ≠ 2 * n^2
 Demostració:
   sorry
